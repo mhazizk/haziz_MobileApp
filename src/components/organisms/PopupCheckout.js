57e12eff61c127c5312a77e76afef0a3653599d1
mhazizk <mhazizk@gmail.com>
2023-04-07 22:15:24 +0700

    Update PopupCheckout.js

--- src/components/organisms/PopupCheckout.js
+++ src/components/organisms/PopupCheckout.js
@@ -1,4 +1,11 @@
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 import React from "react";
 import CartItem from "../molecules/CartItem";
 import CheckoutItem from "../molecules/CheckoutItem";
@@ -6,151 +13,88 @@ import getTotalPrice from "../../utils/getTotalPrice";
 import PrimaryButton from "../atoms/PrimaryButton";
 import CustomTextInput from "../atoms/CustomTextInput";
 import colorsConstants from "../../constants/colorsConstants";
+import IonIcons from "react-native-vector-icons/Ionicons";
+import CloseButton from "../atoms/CloseButton";
+import PopupTitle from "../molecules/PopupTitle";
+import addNumberDelimiterOnlyAndStringify from "../../utils/addNumberDelimiterOnlyAndStringify";
+import InputRow from "../molecules/InputRow";
+import TextRow from "../molecules/TextRow";
 
 const PopupCheckout = ({ cartList, onPressDismiss }) => {
   const [cash, setCash] = React.useState(0);
 
   const change = cash - getTotalPrice(cartList);
 
+  const maxHeight = Dimensions.get("window").height * 0.8;
+
   return (
     <>
       <View
         style={{
-          top: "25%",
-          // bottom: "25%",
-          padding: 16,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          marginHorizontal: 16,
+          paddingHorizontal: 16,
+          paddingTop: 16,
+          paddingBottom: 48,
           alignSelf: "center",
           alignItems: "center",
-          justifyContent: "flex-start",
+          justifyContent: "center",
           position: "absolute",
-          width: "80%",
+          maxHeight: maxHeight,
           zIndex: 10,
-          backgroundColor: "white",
+          backgroundColor: "#ffffff",
           borderRadius: 16,
         }}
       >
-        <View
-          style={{
-            alignItems: "center",
-          }}
-        >
-          <Text
-            style={{
-              fontWeight: "bold",
-              padding: 16,
-              color: colorsConstants.primary,
-            }}
-          >
-            Detail Pesanan
-          </Text>
-        </View>
+        <CloseButton onPress={() => onPressDismiss()} />
+        <PopupTitle title="Detail Pesanan" />
 
-        {cartList.length > 0 &&
-          cartList.map((item) => <CheckoutItem key={item?.id} item={item} />)}
-        <View
-          style={{
-            paddingHorizontal: 8,
-            flexDirection: "row",
-            paddingTop: 16,
+        <ScrollView
+          contentContainerStyle={{
+            width: "100%",
             alignItems: "center",
-            marginBottom: 4,
           }}
         >
-          <Text style={{ fontWeight: "bold", flex: 2 }}>Total</Text>
-          <Text style={{ fontWeight: "bold" }}>:</Text>
-          <View
-            style={{
-              flex: 3,
-              height: 36,
-              alignItems: "flex-end",
-              justifyContent: "center",
-              borderRadius: 8,
-              marginLeft: 8,
+          {cartList.length > 0 &&
+            cartList.map((item) => <CheckoutItem key={item?.id} item={item} />)}
+          <TextRow
+            rowName="Total"
+            paddingTop={16}
+            value={addNumberDelimiterOnlyAndStringify(getTotalPrice(cartList))}
+          />
+          <InputRow
+            rowName="Uang Dibayar"
+            keyboardType="numeric"
+            value={cash}
+            onChange={(number) => {
+              setCash(number);
             }}
-          >
-            <Text
-              style={{
-                fontWeight: "bold",
-                textAlign: "right",
-                paddingRight: 8,
-              }}
-            >
-              {getTotalPrice(cartList)}
-            </Text>
-          </View>
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            paddingHorizontal: 8,
-            alignItems: "center",
-            marginVertical: 4,
-          }}
-        >
-          <Text style={{ fontWeight: "bold", flex: 2 }}>Uang dibayar</Text>
-          <Text style={{ fontWeight: "bold" }}>:</Text>
-          <View
-            style={{
-              flex: 3,
-              borderRadius: 8,
-              overflow: "hidden",
-              marginLeft: 8,
-            }}
-          >
-            <CustomTextInput
-              backgroundColor="#F4F4F4"
-              defaultValue={"0"}
-              value={cash.toString()}
-              onChange={(number) => (number > 0 ? setCash(number) : setCash(0))}
-              keyboardType="numeric"
-            />
-          </View>
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            paddingHorizontal: 8,
-            paddingBottom: 24,
-            alignItems: "center",
-            marginVertical: 4,
-          }}
-        >
-          <Text style={{ fontWeight: "bold", flex: 2 }}>Kembalian</Text>
-          <Text style={{ fontWeight: "bold" }}>:</Text>
-          <View
-            style={{
-              flex: 3,
-              height: 36,
-              alignItems: "flex-end",
-              justifyContent: "center",
-              backgroundColor: change < 0 ? "pink" : "#D6E6D8",
-              borderRadius: 8,
-              marginLeft: 8,
-            }}
-          >
-            <Text
-              style={{
-                fontWeight: "bold",
-                textAlign: "right",
-                color: change < 0 ? "red" : "black",
-                paddingRight: 8,
-              }}
-            >
-              {change}
-            </Text>
-          </View>
-        </View>
+          />
+          <TextRow
+            rowName="Kembalian"
+            paddingBottom={16}
+            backgroundColor={change < 0 ? "pink" : "#D6E6D8"}
+            textColor={change < 0 ? colorsConstants.danger : "black"}
+            value={addNumberDelimiterOnlyAndStringify(change)}
+          />
+        </ScrollView>
+
         <PrimaryButton
           isEnabled={cash >= getTotalPrice(cartList)}
           label="Cetak Struk"
-          onPress={() => {}}
+          onPress={() => alert("Struk berhasil dicetak!")}
         />
       </View>
 
       <TouchableOpacity
         style={{
-          width: "100%",
-          height: "100%",
+          position: "absolute",
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
           backgroundColor: "rgba(0,0,0,0.8)",
           zIndex: 9,
         }}
