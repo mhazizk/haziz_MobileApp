de39bee59b1e660474e1b0b78f40a4860bbb9687
mhazizk <mhazizk@gmail.com>
2023-04-07 22:13:28 +0700

    Update CustomTextInput.js

--- src/components/atoms/CustomTextInput.js
+++ src/components/atoms/CustomTextInput.js
@@ -1,5 +1,8 @@
 import { View, Text, TextInput } from "react-native";
 import React from "react";
+import addNumberDelimiterOnlyAndStringify from "../../utils/addNumberDelimiterOnlyAndStringify";
+import removeDelimiterAndDestringify from "../../utils/removeDelimiterAndDestringify";
+import removeDelimiterAndStringify from "../../utils/removeDelimiterAndStringify";
 
 /**
  *
@@ -13,7 +16,46 @@ const CustomTextInput = ({
   value,
   placeholder,
   onChange,
+  isEnabled = true,
 }) => {
+  let convertedValue = value;
+  const isValueString = typeof value === "string";
+  const isValueNumber = typeof value === "number";
+
+  switch (true) {
+    case keyboardType === "numeric" && isValueNumber:
+      convertedValue = addNumberDelimiterOnlyAndStringify(value);
+      break;
+    case keyboardType === "numeric" && isValueString:
+      convertedValue = addNumberDelimiterOnlyAndStringify(parseInt(value));
+      break;
+    default:
+      break;
+  }
+
+  const inputTextOnChange = (text) => {
+    switch (true) {
+      case keyboardType === "numeric" && isValueNumber:
+        if (text === "" || text < 1) {
+          return onChange(0);
+        }
+        return onChange(removeDelimiterAndDestringify(text));
+      case keyboardType === "numeric" && isValueString:
+        if (text === "" || text < 1) {
+          return onChange("0");
+        }
+        const removedDelimiter = removeDelimiterAndStringify(text);
+        onChange(removedDelimiter);
+        break;
+      case "default":
+        onChange(text);
+        break;
+      default:
+        onChange(text);
+        break;
+    }
+  };
+
   return (
     <>
       <View
@@ -24,6 +66,7 @@ const CustomTextInput = ({
         }}
       >
         <TextInput
+          editable={isEnabled}
           style={{
             width: "100%",
             height: 36,
@@ -34,12 +77,9 @@ const CustomTextInput = ({
           placeholder={placeholder}
           keyboardType={keyboardType}
           defaultValue={defaultValue}
-          value={value}
-          onChangeText={(text) =>
-            onChange(
-              keyboardType === "numeric" ? parseInt(text) : text.toLowerCase()
-            )
-          }
+          numberOfLines={1}
+          value={convertedValue}
+          onChangeText={(text) => inputTextOnChange(text)}
         />
       </View>
     </>
