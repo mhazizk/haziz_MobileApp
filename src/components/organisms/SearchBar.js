33e9942aa005bced6a56eff4b9592440e248da58
mhazizk <mhazizk@gmail.com>
2023-04-07 22:15:29 +0700

    Update SearchBar.js

--- src/components/organisms/SearchBar.js
+++ src/components/organisms/SearchBar.js
@@ -1,22 +1,51 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, Dimensions } from "react-native";
 import React from "react";
 import CustomTextInput from "../atoms/CustomTextInput";
 import colorsConstants from "../../constants/colorsConstants";
+import IonIcons from "react-native-vector-icons/Ionicons";
+import IconButton from "../atoms/IconButton";
 
 const SearchBar = ({ value, onChange }) => {
+  const width = Dimensions.get("window").width - 32;
+
   return (
     <View
       style={{
-        width: "100%",
-        alignItems: "flex-start",
+        width: width,
+        alignItems: "center",
+        marginTop: 16,
+        flexDirection: "row",
         paddingHorizontal: 16,
+        marginHorizontal: 16,
+        backgroundColor: "#ffffff",
+        height: 48,
+        borderRadius: 16,
+        overflow: "hidden",
       }}
     >
+      <IonIcons name="search" size={24} color={colorsConstants.primary} />
       <CustomTextInput
+        placeholder="Cari menu"
         value={value}
         onChange={(text) => onChange(text)}
-        placeholder="Cari menu"
+        keyboardType="default"
       />
+      {value && (
+        <View
+          style={{
+            position: "absolute",
+            right: 16,
+            zIndex: 2,
+          }}
+        >
+          <IconButton
+            name="close"
+            color={colorsConstants.danger}
+            size={24}
+            onPress={() => onChange("")}
+          />
+        </View>
+      )}
     </View>
   );
 };
